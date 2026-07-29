07b21f5d002ae722a62f5acbc7636df6f916aa18
viswanathan <viswanathan34174@gmail.com>
2026-07-29 08:22:13 +0530

    1.2

--- SplitwiseApp.java
+++ SplitwiseApp.java
@@ -4,6 +4,17 @@ public class SplitwiseApp {
 
     public static void main(String[] args){
 
-        System.out.println("Splitwise");
+        System.out.println("=== Splitwise Console ===");
+        System.out.println("Track shared expenses with friends.");
+        System.out.println("More features coming in the next lessons.");
+
+        double totalAmount = 500.0;
+
+        int numFriends =3;
+
+        System.out.println(totalAmount);
+        System.out.println(numFriends);4
+
     }
+
 }
